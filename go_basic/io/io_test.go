352efc9a79c31595c378c2e120eaa5c0ac99293a
jchang118 <josephchang911@msn.com>
2025-08-18 22:17:51 +1000

    buffered.go completed

--- go_basic/io/io_test.go
+++ go_basic/io/io_test.go
@@ -3,6 +3,8 @@ package io_test
 import (
 	"go_tutorial/go_basic/io"
 	"testing"
+	"time"
+	"fmt"
 )
 
 func TestWriteFile(t *testing.T) {
@@ -19,4 +21,13 @@ func TestWriteFileWithBuffer(t *testing.T) {
 
 func TestReadFileWithBuffer(t *testing.T) {
 	io.ReadFileWithBuffer()
+}
+
+func TestBufferedFileWriter(t *testing.T) {
+	t1 := time.Now()
+	io.WriteDirect("../data/no_buffer.txt")
+	t2 := time.Now()
+	io.WriteWithBuffer("../data/with_buffer.txt")
+	t3 := time.Now()
+	fmt.Printf("不用缓冲耗时%dms,用缓冲耗时%dms\n", t2.Sub(t1).Milliseconds(), t3.Sub(t2).Milliseconds())
 }
