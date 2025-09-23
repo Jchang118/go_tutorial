d956ba3d9f42b27e5780e5ac470b73dec864f2b7
jchang118 <josephchang911@msn.com>
2025-09-23 23:21:43 +1000

    TestStdList completed and doc folder added

--- go_frame/data_structure/list_test.go
+++ go_frame/data_structure/list_test.go
@@ -1,6 +1,7 @@
 package data_structure_test
 
 import (
+    "container/list"
     "go_tutorial/go_frame/data_structure"
     "fmt"
     "testing"
@@ -22,4 +23,22 @@ func TestList(t *testing.T) {
     lst.ReverseTraverse()
 }
 
+func TestStdList(t *testing.T) {
+    lst := list.New()
+    lst.PushBack(1)     // 1
+    lst.PushBack(2)     // 1 -> 2
+    lst.PushFront(3)    // 3 -> 1 -> 2
+    lst.PushFront(4)    // 4 -> 3 -> 1 -> 2
+
+    third := lst.Front().Next().Next()  //第三个元素是1
+    lst.InsertAfter(8, third)           // 4 -> 3 -> 1 -> 8 -> 2
+    lst.InsertBefore(9, third)          // 4 -> 3 -> 9 -> 1 -> 8 -> 2
+    lst.Remove(third)                   // 4 -> 3 -> 9 -> 8 -> 2
+
+    fmt.Printf("链表中共有%d个元素\n", lst.Len())
+    data_structure.TraverseList(lst)
+    data_structure.ReverseList(lst)
+}
+
 // go test ./data_structure -v -run=^TestList$ -count=1
+// go test ./data_structure -v -run=^TestStdList$ -count=1
